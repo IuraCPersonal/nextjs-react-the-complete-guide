5bcea5a282a45e70cc120ab2fd180e868a1b7656
IuraCPersonal <iurie.cius.personal@gmail.com>
2023-04-30 11:41:50 +0300

    feat(User): navigating programmatically

--- src/pages/clients/[id]/index.js
+++ src/pages/clients/[id]/index.js
@@ -1,7 +1,23 @@
+import { useRouter } from "next/router";
+
 const ClientProjectsPage = () => {
+  const router = useRouter();
+
+  const loadProjectHandler = () => {
+    // load data...
+    router.push({
+      pathname: '/clients/[id]/[clientprojectid]',
+      query: {
+        id: router.query.id,
+        clientprojectid: 'project_a'
+      }
+    });
+  };
+
   return (
     <div>
-      <h1>The Projects of a given Clien</h1>
+      <h1>The Projects of a given Client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 };
--- src/pages/clients/index.js
+++ src/pages/clients/index.js
@@ -1,16 +1,23 @@
 import Link from "next/link";
 
 const ClientsPage = () => {
+  const clients = [
+    { id: "simpi", name: "Simpi" },
+    { id: "uwu", name: "Zero" },
+  ];
+
   return (
     <div>
       <h1>The Clients Page</h1>
       <ul>
-        <li>
-          <Link href="/clients/simpi">simpi</Link>
-        </li>
-        <li>
-          <Link href="/clients/uwu">Zero</Link>
-        </li>
+        {clients.map(client => (
+          <li key={client.id}>
+            <Link href={{
+              pathname: 'clients/[id]',
+              query: { id: client.id }
+            }}>{client.name}</Link>
+          </li>
+        ))}
       </ul>
     </div>
   );
